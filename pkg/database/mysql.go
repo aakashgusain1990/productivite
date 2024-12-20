b8cb35dfa1c1e9ecea35f003ff433ea1c4d65aa6
Akash Gusain <77015094+aakashgusain1990@users.noreply.github.com>
2024-12-20 23:27:22 +0530

    Update pkg/database/mysql.go

--- pkg/database/mysql.go
+++ pkg/database/mysql.go
@@ -12,7 +12,7 @@ var (
 
 func NewMySQLDatabase(conf *entities.Config) *MySQLDatabase {
 	once.Do(func() {
-		sqlDB, err := sql.Open("mysql", "")
+		sqlDB, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", conf.DBUser, conf.DBPassword, conf.DBHost, conf.DBPort, conf.DBName))
 		if err != nil {
 			panic(err)
 		}
